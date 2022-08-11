08d218df1e940bbf6cc4356d54efdd9a706c2453
uniRomitoskj <romito.1932500@studenti.uniroma1.it>
2022-08-11 19:49:42 +0200

    ddl

--- db/ddl.sql
+++ db/ddl.sql
@@ -1,11 +1,12 @@
 CREATE TABLE IF NOT EXISTS Utente (
-    id INT UNSIGNED AUTO_INCREMENT,
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(30) NOT NULL,
-    password CHAR(60) NOT NULL -- output length of the implemented hash function
+    password CHAR(60) NOT NULL,
+    -- output length of the implemented hash function
     cognome VARCHAR(30) NOT NULL,
     nome VARCHAR(30) NOT NULL,
     data_nascita DATE NOT NULL,
     sesso ENUM('M', 'F') NOT NULL,
-    email VARCHAR(50) CHECK(email REGEXP '^[\w-\.]+\w@([\w-]+\.)+[\w-]{2,4}$'),
+    email VARCHAR(50) CHECK(email REGEXP '^\w[\w\-\.]+\w@([\w\-]+\.)+[\w]+'),
     telefono VARCHAR(15) CHECK(telefono REGEXP '^[+]?([0-9]{6}[0-9]*)$')
 )
